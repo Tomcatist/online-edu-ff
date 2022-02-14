132a75b8f5723a0b7ccf1a8ecd2e3b63aad63f91
tommy <18232336174@163.com>
2022-02-14 23:09:36 +0800

    登录注册，微信扫码

--- utils/request.js
+++ utils/request.js
@@ -1,7 +1,22 @@
 import axios from 'axios'
+import cookie from "js-cookie";
 // 创建axios实例
 const service = axios.create({
   baseURL: 'http://localhost:9001', // api的base_url
   timeout: 20000 // 请求超时时间
 })
+
+service.interceptors.request.use(
+  config => {
+    // debugger
+    if (cookie.get('yuan_token')) {
+      config.headers['token'] = cookie.get('yuan_token');
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err);
+  }
+)
+
 export default service
